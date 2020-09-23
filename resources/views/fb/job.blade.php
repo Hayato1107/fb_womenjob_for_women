01a64a417ebc1ca6dbc58d1cf8db4901cf978210
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2020-09-23 19:38:41 +0000

    Mm

--- resources/views/fb/job.blade.php
+++ resources/views/fb/job.blade.php
@@ -15,6 +15,7 @@
         
         <div class="big-body">
             <div class="body-top">
+                <img src="{{ asset('image/lp_instagram_job.png') }}" class="wide">
                 <img src="{{ asset('image/jobManga_1.png') }}" class="wide">
                 <img src="{{ asset('image/jobManga_2.png') }}" class="wide">
                 <h1><span>求人見つけたらラッキー!?</span></h1>
