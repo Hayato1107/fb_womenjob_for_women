eabf9612c16ad389aecc61739bf1c20ee9a949c3
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2020-12-16 19:35:29 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,16 @@ Route::get('os', 'AdController@os');
 Route::get('cccheck', function () {
     return view('fb.job'); //獲得LP(1)
 });
+
+
+//YouTube広告 - リダイレクト(テスト)
+// LP獲得用
+Route::get('works', function () {
+    return view('fb.check'); //確認用LP
+    // return redirect('https://a-trade.jp/redirect/biscor?media=G16531'); //ビスコール
+});
+
+// LP確認用
+Route::get('cccheck2', function () {
+    return redirect('https://a-trade.jp/redirect/biscor?media=G16531'); //ビスコール
+});
