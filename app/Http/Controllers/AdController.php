941a9ebb80326ffa0bdc2195f9c26b0b8ada4472
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2021-01-27 00:45:35 +0000

    mM

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -11,9 +11,9 @@ class AdController extends Controller
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
             // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
-            return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON'); //トークウィズ - Preaf
+            return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
         }
-        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16216'); //トリトモ - fam
+        return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
         // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
     }
 }
