18a5592550be3cbbf1b5e829b0da8d07f58c1953
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2021-01-31 07:15:09 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -11,9 +11,9 @@ class AdController extends Controller
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
             // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
-            return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
+            return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         }
-        return redirect('https://a-trade.jp/redirect/fatey-multi?media=G16531'); //FATEY - Aトレード
+        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
     }
 }
