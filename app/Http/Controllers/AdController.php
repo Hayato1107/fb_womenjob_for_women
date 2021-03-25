f299e646a2ddd2d964ef6ffaf58c427e69446c48
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2021-03-25 01:48:46 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,10 +10,12 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://track.bannerbridge.net/click.php?APID=143797&affID=98211&siteID=189838'); //Jメール - バナブリ
+            return redirect('https://track.bannerbridge.net/click.php?APID=136868&affID=98211&siteID=189838'); //Jメール - バナブリ
+            // return redirect('https://track.bannerbridge.net/click.php?APID=143797&affID=98211&siteID=189838'); //Jメール特単 - バナブリ
             // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
         }
         // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
-        return redirect('https://track.bannerbridge.net/click.php?APID=143797&affID=98211&siteID=189838'); //Jメール - バナブリ
+        // return redirect('https://track.bannerbridge.net/click.php?APID=143797&affID=98211&siteID=189838'); //Jメール特単 - バナブリ
+        return redirect('https://track.bannerbridge.net/click.php?APID=136868&affID=98211&siteID=189838'); //Jメール - バナブリ
     }
 }
