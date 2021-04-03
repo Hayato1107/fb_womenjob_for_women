5e76052ffff6d3cb185cf9c3a380ae4a350c6fd9
EC2 Default User <ec2-user@ip-172-31-21-249.us-east-2.compute.internal>
2021-04-03 19:28:32 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// // LP獲得用
-// Route::get('guy1', function () {
-//     return view('fb.check'); //確認用LP
-//     // cccheckを獲得LP(guy1)へ
+// LP獲得用
+Route::get('guy1', function () {
+    return view('fb.check'); //確認用LP
+    // cccheckを獲得LP(guy1)へ
     
-//     // return view('fb.adultad'); //YouTube - アダルト大人出会い訴求
-//     // return redirect('http://www.chatpia.jp/recruit/sp/?b=G16531'); //チャットピア - Aトレード
-//     // return view('fb.job'); //獲得LP(1) - チャットレディーweb
-//     // return view('fb.adultad'); //YouTube - アダルト大人訴求
-//     // return view('fb.mama'); //獲得LP - ママ活
-// });
+    // return view('fb.adultad'); //YouTube - アダルト大人出会い訴求
+    // return redirect('http://www.chatpia.jp/recruit/sp/?b=G16531'); //チャットピア - Aトレード
+    // return view('fb.job'); //獲得LP(1) - チャットレディーweb
+    // return view('fb.adultad'); //YouTube - アダルト大人訴求
+    // return view('fb.mama'); //獲得LP - ママ活
+});
 
 // OS識別x遷移
-Route::get('guy1', 'AdController@os');
+Route::get('cccheck', 'AdController@os');
 
 // // LP確認用
 // Route::get('cccheck', function () {
